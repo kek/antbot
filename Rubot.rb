aafd7736d066f619a63ad87c3b8024859f81157b
Karl Eklund <localpart@gmail.com>
2012-03-27 00:48:19 +0200

    New algorithm

--- Rubot.rb
+++ Rubot.rb
@@ -2,8 +2,7 @@ $:.unshift File.dirname($0)
 require 'ants.rb'
 
 ai=AI.new
-orders = {}
-AVOID_TIME = 20
+history = {}
 
 ai.setup do |ai|
   # your setup code here, if any
@@ -12,29 +11,82 @@ end
 ai.run do |ai|
   current_orders = {}
 
-  ai.my_ants.each do |ant|
-    directions = [:N, :E, :S, :W]
-    directions = directions.sort { |x,y| rand - 0.5 }
+  ai.my_ants.each_with_index do |ant, i|
+    $stderr.puts "\nAnt #{i}:"
 
-    all_blocked = directions.reject { |dir|
-      loc = ant.square.neighbor(dir)
-
-      !loc.land? or
-      (orders[loc] and (orders[loc] > ai.turn_number - AVOID_TIME + AVOID_TIME/4)) or
-      ant.square.neighbor(dir).ant?
-    }.empty?
-
-    directions.each do |dir|
-      loc = ant.square.neighbor(dir)
-      if loc.land? and
-          (all_blocked or !(orders[loc] and (orders[loc] > ai.turn_number - AVOID_TIME))) and
-          not current_orders[loc] and
-          not loc.ant?
-        orders[loc] = ai.turn_number unless all_blocked
-        current_orders[loc] = true
-        ant.order dir
-        break
+    scores = [:N, :E, :S, :W].map { |dir|
+      dest = ant.square.neighbor(dir)
+      if !dest.land?
+        score = nil
+      elsif history[dest]
+        # Higher age is good
+        score = ai.turn_number - history[dest]
+      else
+        # Untravelled is best choice
+        score = 1001
       end
+      $stderr.puts "ant #{i} - #{dir} - score #{score or 'nil'}" unless !score
+      { dir => score }
+    }
+
+    scores = scores.reduce(Hash.new) { |memo, obj|
+      memo.merge obj
+    }
+
+    # $stderr.puts "scores for ant #{i}:"
+    # scores.each_pair do |key, value|
+    #   $stderr.puts "direction #{key} - score #{value}"
+    # end
+
+    directions = [:N, :E, :S, :W].sort {|x| 0.5 <=> rand }.reject { |dir|
+      scores[dir] == nil
+    }.sort { |x,y|
+      scores[x] <=> scores[y]
+    }.reverse
+
+    $stderr.puts "Possible moves, in order:"
+    directions.each {|dir|
+      $stderr.puts "#{dir} - #{scores[dir]}"
+    }
+
+    if directions.length > 0
+      dir = directions.first
+
+      $stderr.puts "ant #{i} going #{dir}"
+
+      dest = ant.square.neighbor(dir)
+
+      if !current_orders[dest] and !dest.ant?
+        ant.order dir
+        current_orders[dest] = true
+        history[dest] = ai.turn_number
+      else
+        $stderr.puts "Blocked"
+      end
+    else
+      $stderr.puts "No good move for ant #{i}"
     end
+
+
+    # all_blocked = directions.reject { |dir|
+    #   loc = ant.square.neighbor(dir)
+
+    #   !loc.land? or
+    #   (history[loc] and (history[loc] > ai.turn_number - AVOID_TIME + AVOID_TIME/4)) or
+    #   ant.square.neighbor(dir).ant?
+    # }.empty?
+
+    # directions.each do |dir|
+    #   loc = ant.square.neighbor(dir)
+    #   if loc.land? and
+    #       (all_blocked or !(history[loc] and (history[loc] > ai.turn_number - AVOID_TIME))) and
+    #       not current_orders[loc] and
+    #       not loc.ant?
+    #     history[loc] = ai.turn_number unless all_blocked
+    #     current_orders[loc] = true
+    #     ant.order dir
+    #     break
+    #   end
+    # end
   end
 end
